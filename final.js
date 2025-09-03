541d3dbbc4bd5302e2bba9b160142261b08b333e
muhib003 <muhib.kagozi03@gmail.com>
2025-09-03 20:14:37 +0600

    first and second problem is done

---
+++ final.js
@@ -0,0 +1,18 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 2060;
+//write your code here
+var finalAsset = (area / 2);
+console.log(finalAsset);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 30000;
+//write your code here
+if(money >= 25000){
+    console.log("Laptop");
+}
+else if(money >=10000 && money < 25000){
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
